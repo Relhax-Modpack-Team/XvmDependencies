733599912be0b4d615f97b27e632bc4cf673cee2
elektrosmoker <40739360+elektrosmoker@users.noreply.github.com>
2019-12-02 14:39:04 +0100

    added Dependency_PY_aiming/_anglesAiming/_maps/_markerPosition/_rewindRepeat/_startBattle/_timer

---
+++ PY/Dependency_XVM_PY_startBattle/res_mods/configs/xvm/py_macro/startBattle.py
@@ -0,0 +1,30 @@
+from xfw import *
+from gui.Scaleform.daapi.view.battle.shared.battle_timers import BattleTimer
+from Vehicle import Vehicle
+import BigWorld
+
+
+startBattle = 0
+arenaPeriod = None
+
+
+@registerEvent(BattleTimer, 'setTotalTime')
+def setTotalTime(self, totalTime):
+    global startBattle
+    startBattle = arenaPeriod.getPeriod() if arenaPeriod is not None else 0
+    as_event('ON_BATTLE_TIMER')
+
+
+@registerEvent(Vehicle, 'onEnterWorld')
+def onEnterWorld(self, prereqs):
+    global startBattle, arenaPeriod
+    if self.isPlayerVehicle:
+        startBattle = 0
+        player = BigWorld.player()
+        arenaPeriod = player.guiSessionProvider.shared.arenaPeriod
+
+
+@xvm.export('xvm.startBattle', deterministic=False)
+def xvm_startBattle():
+    return startBattle
+
---
+++ PY/Dependency_XVM_PY_timer/res_mods/configs/xvm/py_macro/timer.py
@@ -0,0 +1,20 @@
+from xfw import *
+from Vehicle import Vehicle
+import BigWorld
+
+timeBeginBattle = 0.0
+
+
+@registerEvent(Vehicle, 'onEnterWorld')
+def onEnterWorld(self, prereqs):
+    global timeBeginBattle
+    if self.isPlayerVehicle:
+        timeBeginBattle = BigWorld.serverTime()
+
+
+@xvm.export('xvm.leftTime', deterministic=False)
+def xvm_leftTime(lt=0):
+    global timeBeginBattle
+    if timeBeginBattle == 0.0:
+        timeBeginBattle = BigWorld.serverTime()
+    return 'lt' if (BigWorld.serverTime() - timeBeginBattle) < lt else None
