6db2f6cca9a93539abd8d59feddcb6e9dd5b9bd1
elektrosmoker <40739360+elektrosmoker@users.noreply.github.com>
2020-01-28 20:07:35 +0100

    Update chancePenetration.py

--- PY/Dependency_XVM_PY_chancePenetration/res_mods/configs/xvm/py_macro/chancePenetration.py
+++ PY/Dependency_XVM_PY_chancePenetration/res_mods/configs/xvm/py_macro/chancePenetration.py
@@ -4,9 +4,8 @@ from Vehicle import Vehicle
 from gui.Scaleform.daapi.view.battle.classic.stats_exchange import FragsCollectableStats
 from AvatarInputHandler.gun_marker_ctrl import _CrosshairShotResults, _SHOT_RESULT
 import gui.Scaleform.daapi.view.battle.shared.crosshair.plugins as plug
-from math import erfc
 
-from xfw import *
+from xfw.events import registerEvent, overrideMethod, overrideClassMethod
 from xvm_main.python.logger import *
 import xvm_main.python.config as config
 from xfw_actionscript.python import *
@@ -29,23 +28,20 @@ playerVehicleID = None
 
 
 @registerEvent(plug.ShotResultIndicatorPlugin, '_ShotResultIndicatorPlugin__onGunMarkerStateChanged')
-def onGunMarkerStateChanged(self, markerType, position, dir, collision):
+def onGunMarkerStateChanged(self, markerType, position, direction, collision):
     if config.get('sight/enabled', True) and battle.isBattleTypeSupported:
         if not self._ShotResultIndicatorPlugin__isEnabled:
-             self._ShotResultIndicatorPlugin__shotResultResolver.getShotResult(position, collision, dir, excludeTeam=self._ShotResultIndicatorPlugin__playerTeam)
+            self._ShotResultIndicatorPlugin__shotResultResolver.getShotResult(position, collision, direction, excludeTeam=self._ShotResultIndicatorPlugin__playerTeam)
 
 
 @overrideClassMethod(_CrosshairShotResults, 'getShotResult')
-def _CrosshairShotResults_getShotResult(base, cls, hitPoint, collision, dir, excludeTeam=0):
+def _CrosshairShotResults_getShotResult(base, cls, hitPoint, collision, direction, excludeTeam=0):
     if config.get('sight/enabled', True) and battle.isBattleTypeSupported:
-        def updateLabel():
-            if (old_armorActual != armorActual) or (old_piercingActual != piercingActual):
-                as_event('ON_CALC_ARMOR')
-
         global piercingActual, armorActual, shotResult, hitAngle, normHitAngle, piercingChance
         keyColor = [None, 'not_pierced', 'little_pierced', 'great_pierced']
         old_piercingActual = piercingActual
         old_armorActual = armorActual
+        old_hitAngle = hitAngle
         piercingActual = None
         armorActual = None
         piercingChance = None
@@ -53,90 +49,91 @@ def _CrosshairShotResults_getShotResult(base, cls, hitPoint, collision, dir, exc
         normHitAngle = None
         shotResult = keyColor[0]
         if collision is None:
-            updateLabel()
             return _SHOT_RESULT.UNDEFINED
-        else:
-            entity = collision.entity
-            if entity.__class__.__name__ not in ('Vehicle', 'DestructibleEntity'):
-                updateLabel()
-                return _SHOT_RESULT.UNDEFINED
-            if entity.health <= 0 or entity.publicInfo['team'] == excludeTeam:
-                updateLabel()
-                return _SHOT_RESULT.UNDEFINED
-            player = BigWorld.player()
-            if player is None:
-                updateLabel()
-                return _SHOT_RESULT.UNDEFINED
-            vDesc = player.getVehicleDescriptor()
-            shell = vDesc.shot.shell
-            caliber = shell.caliber
-            shellKind = shell.kind
-            ppDesc = vDesc.shot.piercingPower
-            maxDist = vDesc.shot.maxDistance
-            dist = (hitPoint - player.getOwnVehiclePosition()).length
-            piercingPower = cls._computePiercingPowerAtDist(ppDesc, dist, maxDist)
-            fullPiercingPower = piercingPower
-            minPP, maxPP = cls._computePiercingPowerRandomization(shell)
-            result = _SHOT_RESULT.NOT_PIERCED
-            isJet = False
-            jetStartDist = None
-            ignoredMaterials = set()
-            collisionsDetails = cls._getAllCollisionDetails(hitPoint, dir, entity)
-            if collisionsDetails is None:
-                updateLabel()
-                return _SHOT_RESULT.UNDEFINED
-            for cDetails in collisionsDetails:
-                if isJet:
-                    jetDist = cDetails.dist - jetStartDist
-                    if jetDist > 0.0:
-                        piercingPower *= 1.0 - jetDist * cls._SHELL_EXTRA_DATA[shellKind].jetLossPPByDist
-                if cDetails.matInfo is None:
-                    result = cls._CRIT_ONLY_SHOT_RESULT
-                else:
-                    matInfo = cDetails.matInfo
-                    if (cDetails.compName, matInfo.kind) in ignoredMaterials:
-                        continue
-                    hitAngleCos = cDetails.hitAngleCos if matInfo.useHitAngle else 1.0
-                    hitAngle = hitAngleCos
-                    normHitAngle = hitAngle
-                    if not isJet and cls._shouldRicochet(shellKind, hitAngleCos, matInfo, caliber):
-                        normHitAngle = -1.0
-                        break
-                    piercingPercent = 1000.0
-                    if piercingPower > 0.0:
-                        penetrationArmor = cls._computePenetrationArmor(shellKind, hitAngleCos, matInfo, caliber)
-                        normHitAngle = matInfo.armor / penetrationArmor if penetrationArmor != 0.0 else hitAngle
-                        piercingPercent = 100.0 + (penetrationArmor - piercingPower) / fullPiercingPower * 100.0
-                        piercingActual = int(piercingPower)
-                        armorActual = int(penetrationArmor)
-                        # piercingChance = max(0, min(1.0, (piercingPower / penetrationArmor - 0.75) * 2)) if penetrationArmor > 0.0 else 1.0
-                        armorRatio = float(penetrationArmor) / piercingPower - 1
-                        piercingChance = 1.0 if armorRatio < -0.25 else 0.5 * erfc(8.485281374238576 * armorRatio ) if armorRatio <= 0.25 else 0.0
-                        piercingPower -= penetrationArmor
-                        # log('penetrationArmor = %s     piercingPercent = %s    piercingPower = %s' % (penetrationArmor, piercingPercent, piercingPower))
-                    if matInfo.vehicleDamageFactor:
-                        if minPP < piercingPercent < maxPP:
-                            result = _SHOT_RESULT.LITTLE_PIERCED
-                        elif piercingPercent <= minPP:
-                            result = _SHOT_RESULT.GREAT_PIERCED
-                        break
-                    elif matInfo.extra:
-                        if piercingPercent <= maxPP:
-                            result = cls._CRIT_ONLY_SHOT_RESULT
-                    if matInfo.collideOnceOnly:
-                        ignoredMaterials.add((cDetails.compName, matInfo.kind))
-                if piercingPower <= 0.0:
+        entity = collision.entity
+        if entity.__class__.__name__ not in ('Vehicle', 'DestructibleEntity'):
+            return _SHOT_RESULT.UNDEFINED
+        if entity.health <= 0 or entity.publicInfo['team'] == excludeTeam:
+            return _SHOT_RESULT.UNDEFINED
+        player = BigWorld.player()
+        if player is None:
+            return _SHOT_RESULT.UNDEFINED
+        vDesc = player.getVehicleDescriptor()
+        shell = vDesc.shot.shell
+        caliber = shell.caliber
+        shellKind = shell.kind
+        ppDesc = vDesc.shot.piercingPower
+        maxDist = vDesc.shot.maxDistance
+        dist = (hitPoint - player.getOwnVehiclePosition()).length
+        piercingPower = cls._computePiercingPowerAtDist(ppDesc, dist, maxDist)
+        fullPiercingPower = piercingPower
+        minPP, maxPP = cls._computePiercingPowerRandomization(shell)
+        result = _SHOT_RESULT.NOT_PIERCED
+        isJet = False
+        jetStartDist = None
+        ignoredMaterials = set()
+        collisionsDetails = cls._getAllCollisionDetails(hitPoint, direction, entity)
+        if collisionsDetails is None:
+            return _SHOT_RESULT.UNDEFINED
+        for cDetails in collisionsDetails:
+            if isJet:
+                jetDist = cDetails.dist - jetStartDist
+                if jetDist > 0.0:
+                    piercingPower *= 1.0 - jetDist * cls._SHELL_EXTRA_DATA[shellKind].jetLossPPByDist
+            if cDetails.matInfo is None:
+                result = cls._CRIT_ONLY_SHOT_RESULT
+            else:
+                matInfo = cDetails.matInfo
+                if (cDetails.compName, matInfo.kind) in ignoredMaterials:
+                    continue
+                hitAngleCos = cDetails.hitAngleCos if matInfo.useHitAngle else 1.0
+                hitAngle = hitAngleCos
+                normHitAngle = hitAngle
+                if not isJet and cls._shouldRicochet(shellKind, hitAngleCos, matInfo, caliber):
+                    normHitAngle = -1.0
                     break
-                if cls._SHELL_EXTRA_DATA[shellKind].jetLossPPByDist > 0.0:
-                    isJet = True
-                    mInfo = cDetails.matInfo
-                    armor = mInfo.armor if mInfo is not None else 0.0
-                    jetStartDist = cDetails.dist + armor * 0.001
-            shotResult = keyColor[result]
-            updateLabel()
-            return result
+                piercingPercent = 1000.0
+                if piercingPower > 0.0:
+                    penetrationArmor = cls._computePenetrationArmor(shellKind, hitAngleCos, matInfo, caliber)
+                    normHitAngle = matInfo.armor / penetrationArmor if penetrationArmor != 0.0 else hitAngle
+                    piercingPercent = 100.0 + (penetrationArmor - piercingPower) / fullPiercingPower * 100.0
+                    piercingActual = int(piercingPower)
+                    armorActual = int(penetrationArmor)
+                    # piercingChance = max(0, min(1.0, (piercingPower / penetrationArmor - 0.75) * 2)) if penetrationArmor > 0.0 else 1.0
+                    armorRatio = penetrationArmor / piercingPower - 1.0
+                    # piercingChance = 1.0 if armorRatio < -0.25 else 0.5 * math.erfc(8.485281374238576 * armorRatio) if armorRatio <= 0.25 else 0.0
+                    if armorRatio < -0.25:
+                        piercingChance = 1.0
+                    elif armorRatio > 0.25:
+                        piercingChance = 0.0
+                    else:
+                        piercingChance = 0.5 * math.erfc(8.485281374238576 * armorRatio)
+                    piercingPower -= penetrationArmor
+                    # log('penetrationArmor = %s     piercingPercent = %s    piercingPower = %s' % (penetrationArmor, piercingPercent, piercingPower))
+                if matInfo.vehicleDamageFactor:
+                    if minPP < piercingPercent < maxPP:
+                        result = _SHOT_RESULT.LITTLE_PIERCED
+                    elif piercingPercent <= minPP:
+                        result = _SHOT_RESULT.GREAT_PIERCED
+                    break
+                elif matInfo.extra:
+                    if piercingPercent <= maxPP:
+                        result = cls._CRIT_ONLY_SHOT_RESULT
+                if matInfo.collideOnceOnly:
+                    ignoredMaterials.add((cDetails.compName, matInfo.kind))
+            if piercingPower <= 0.0:
+                break
+            if cls._SHELL_EXTRA_DATA[shellKind].jetLossPPByDist > 0.0:
+                isJet = True
+                mInfo = cDetails.matInfo
+                armor = mInfo.armor if mInfo is not None else 0.0
+                jetStartDist = cDetails.dist + armor * 0.001
+        shotResult = keyColor[result]
+        if (old_armorActual != armorActual) or (old_piercingActual != piercingActual) or (old_hitAngle != hitAngle):
+            as_event('ON_CALC_ARMOR')
+        return result
     else:
-        return base(hitPoint, collision, dir, excludeTeam)
+        return base(hitPoint, collision, direction, excludeTeam)
 
 
 @registerEvent(Vehicle, 'onEnterWorld')
@@ -149,7 +146,8 @@ def Vehicle_onEnterWorld(self, prereqs):
         shotResult = None
         hitAngle = None
         normHitAngle = None
-        colorPiercingChance = config.get('sight/c_piercingChance', COLOR_PIERCING_CHANCE)
+        colorPiercingChance = config.get(
+            'sight/c_piercingChance', COLOR_PIERCING_CHANCE)
         playerVehicleID = self.id
         as_event('ON_CALC_ARMOR')
 
