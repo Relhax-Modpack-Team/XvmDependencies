644e6b28de038311c3cdb2252f5e676791514d33
elektrosmoker <40739360+elektrosmoker@users.noreply.github.com>
2020-01-28 19:56:31 +0100

    Update rewindRepeat.py

--- PY/Dependency_XVM_PY_rewindRepeat/res_mods/configs/xvm/py_macro/rewindRepeat.py
+++ PY/Dependency_XVM_PY_rewindRepeat/res_mods/configs/xvm/py_macro/rewindRepeat.py
@@ -3,7 +3,7 @@ import Keys
 from BattleReplay import BattleReplay, REPLAY_TIME_MARK_REPLAY_FINISHED, REPLAY_TIME_MARK_CURRENT_TIME, FAST_FORWARD_STEP
 from helpers import isPlayerAvatar
 
-from xfw import *
+from xfw.events import overrideMethod
 from xvm_main.python.logger import *
 
 
@@ -15,6 +15,7 @@ accelerate = {Keys.KEY_NONE: 1,
 
 @overrideMethod(BattleReplay, 'handleKeyEvent')
 def BattleReplay_handleKeyEvent(base, self, isDown, key, mods, isRepeat, event):
+    # , Keys.KEY_DELETE]:
     if key not in [Keys.KEY_LEFTARROW, Keys.KEY_RIGHTARROW]:
         return base(self, isDown, key, mods, isRepeat, event)
     if not self.isPlaying:
@@ -32,6 +33,12 @@ def BattleReplay_handleKeyEvent(base, self, isDown, key, mods, isRepeat, event):
         self._BattleReplay__isMenuShowed = False
     if self._BattleReplay__isMenuShowed:
         return False
+    # if key == Keys.KEY_DELETE and isDown and not self._BattleReplay__isFinished:
+    #     if self._BattleReplay__playbackSpeedIdx > 0:
+    #         self.setPlaybackSpeedIdx(0)
+    #     else:
+    #         self.setPlaybackSpeedIdx(self._BattleReplay__savedPlaybackSpeedIdx if self._BattleReplay__savedPlaybackSpeedIdx != 0 else self._BattleReplay__playbackSpeedModifiers.index(1.0))
+    #     return True
     currReplayTime = self._BattleReplay__replayCtrl.getTimeMark(REPLAY_TIME_MARK_CURRENT_TIME)
     finishReplayTime = self._BattleReplay__replayCtrl.getTimeMark(REPLAY_TIME_MARK_REPLAY_FINISHED)
     if currReplayTime > finishReplayTime:
