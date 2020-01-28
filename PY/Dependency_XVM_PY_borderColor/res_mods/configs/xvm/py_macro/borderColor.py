644742572bac0acaa49cb429e18c8885ce0849de
elektrosmoker <40739360+elektrosmoker@users.noreply.github.com>
2020-01-28 19:53:36 +0100

    Update borderColor.py

--- PY/Dependency_XVM_PY_borderColor/res_mods/configs/xvm/py_macro/borderColor.py
+++ PY/Dependency_XVM_PY_borderColor/res_mods/configs/xvm/py_macro/borderColor.py
@@ -1,7 +1,8 @@
 from gui.battle_control.controllers.arena_border_ctrl import ArenaBorderController
 
-from xfw import *
+from xfw.events import overrideMethod
 import xvm_main.python.config as config
+from xvm_main.python.logger import *
 
 
 COLOR = "0x00FF00"
@@ -9,6 +10,7 @@ ALPHA = 100
 
 @overrideMethod(ArenaBorderController, '_ArenaBorderController__getCurrentColor')
 def ArenaBorderController__getCurrentColor(base, self, colorBlind):
+    # log('ArenaBorderController = %s' % base(self, colorBlind))
     color = int(config.get('battle/borderColor/color', COLOR), 16)
     alpha = int(config.get('battle/borderColor/alpha', ALPHA))
     red = ((color & 0xff0000) >> 16) / 255.0
@@ -16,3 +18,4 @@ def ArenaBorderController__getCurrentColor(base, self, colorBlind):
     blue = (color & 0x0000ff) / 255.0
     alpha = alpha / 100.0
     return red, green, blue, alpha
+
