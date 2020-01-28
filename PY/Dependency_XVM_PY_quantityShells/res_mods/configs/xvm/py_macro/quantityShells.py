03e0a28d96a14d48d8d5dd1b7faf4afa5ce44e61
elektrosmoker <40739360+elektrosmoker@users.noreply.github.com>
2020-01-28 20:05:19 +0100

    Update quantityShells.py

--- PY/Dependency_XVM_PY_quantityShells/res_mods/configs/xvm/py_macro/quantityShells.py
+++ PY/Dependency_XVM_PY_quantityShells/res_mods/configs/xvm/py_macro/quantityShells.py
@@ -1,8 +1,9 @@
 from Vehicle import Vehicle
 from gui.Scaleform.daapi.view.battle.classic.stats_exchange import FragsCollectableStats
 from gui.Scaleform.daapi.view.meta.CrosshairPanelContainerMeta import CrosshairPanelContainerMeta
+from gui.Scaleform.daapi.view.battle.shared.vehicles.dualgun_component import DualGunComponent
 
-from xfw import *
+from xfw.events import registerEvent, overrideMethod
 from xvm_main.python.logger import *
 import xvm_main.python.config as config
 from xfw_actionscript.python import *
@@ -14,11 +15,22 @@ quantityInClipShells = None
 quantityInClipShellsMax = None
 burst = None
 playerVehicleID = None
+isAlive = True
+dualGunState = [False, False]
+isDualgunVehicle = False
+
+
+@registerEvent(DualGunComponent, '_DualGunComponent__updateGunState')
+def DualGunComponent__updateGunState(self, gunID, state, serverCooldownData):
+    global dualGunState, quantityInClipShells
+    dualGunState[gunID] = (state == 3)
+    quantityInClipShells = dualGunState[0] + dualGunState[1]
+    as_event('ON_AMMO_COUNT')
 
 
 @registerEvent(CrosshairPanelContainerMeta, 'as_setAmmoStockS')
 def CrosshairPanelContainerMeta_as_setAmmoStockS(self, quantity, quantityInClip, isLow, clipState, clipReloaded):
-    if config.get('sight/enabled', True) and battle.isBattleTypeSupported:
+    if config.get('sight/enabled', True) and battle.isBattleTypeSupported and isAlive:
         global quantityShells, quantityInClipShells
         quantityShells = max(quantity, 0)
         quantityInClipShells = max(quantityInClip, 0) if quantityInClipShellsMax > 1 else None
@@ -27,21 +39,33 @@ def CrosshairPanelContainerMeta_as_setAmmoStockS(self, quantity, quantityInClip,
 
 @registerEvent(Vehicle, 'onEnterWorld')
 def Vehicle_onEnterWorld(self, prereqs):
-    global quantityShells, quantityInClipShells, burst, quantityInClipShellsMax, playerVehicleID
+    global quantityShells, quantityInClipShells, burst, quantityInClipShellsMax, playerVehicleID, isAlive, isDualgunVehicle, dualGunState
     if self.isPlayerVehicle and config.get('sight/enabled', True) and battle.isBattleTypeSupported:
+        isAlive = self.isAlive()
         quantityShells = None
         quantityInClipShells = None
-        gun = self.typeDescriptor.gun
-        quantityInClipShellsMax = gun.clip[0]
-        burst = gun.burst[0]
-        playerVehicleID = self.id
+        dualGunState = [False, False]
+        isDualgunVehicle = self.typeDescriptor.isDualgunVehicle
+        if isAlive:
+            if isDualgunVehicle:
+                quantityInClipShellsMax = 2
+                burst = 1
+            else:
+                gun = self.typeDescriptor.gun
+                quantityInClipShellsMax = gun.clip[0]
+                burst = gun.burst[0]
+            playerVehicleID = self.id
+        else:
+            quantityInClipShellsMax = None
+            burst = None
         as_event('ON_AMMO_COUNT')
 
 
 @registerEvent(FragsCollectableStats, 'addVehicleStatusUpdate')
 def FragsCollectableStats_addVehicleStatusUpdate(self, vInfoVO):
-    global quantityShells, quantityInClipShells, burst, quantityInClipShellsMax
+    global quantityShells, quantityInClipShells, burst, quantityInClipShellsMax, isAlive
     if (not vInfoVO.isAlive()) and (playerVehicleID == vInfoVO.vehicleID) and battle.isBattleTypeSupported:
+        isAlive = False
         quantityShells = None
         quantityInClipShells = None
         quantityInClipShellsMax = None
@@ -66,7 +90,12 @@ def sight_quantityInClipShellsMax():
 
 @xvm.export('sight.isFullClipShells', deterministic=False)
 def sight_isFullClipShells():
-    return 'full' if quantityInClipShellsMax is not None and quantityInClipShellsMax is not None and (quantityInClipShellsMax == quantityInClipShells) else None
+    result = (dualGunState[0] and dualGunState[1]) if isDualgunVehicle else (quantityInClipShellsMax is not None and (quantityInClipShellsMax == quantityInClipShells))
+    return 'full' if result else None
+    # if isDualgunVehicle:
+    #     return 'full' if dualGunState[0] and dualGunState[1] else None
+    # else:
+    #     return 'full' if quantityInClipShellsMax is not None and (quantityInClipShellsMax == quantityInClipShells) else None
 
 
 @xvm.export('sight.burst', deterministic=False)
