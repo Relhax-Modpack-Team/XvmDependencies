7726a86e87608857cd86f3583423096c66aa45ca
elektrosmoker <40739360+elektrosmoker@users.noreply.github.com>
2020-03-04 10:19:36 +0100

    Update aimingSystem.py

--- PY/Dependency_XVM_PY_aimingSystem/res_mods/configs/xvm/py_macro/aimingSystem.py
+++ PY/Dependency_XVM_PY_aimingSystem/res_mods/configs/xvm/py_macro/aimingSystem.py
@@ -4,8 +4,9 @@ from Avatar import PlayerAvatar
 from AvatarInputHandler.AimingSystems.ArcadeAimingSystem import ArcadeAimingSystem
 from AvatarInputHandler.AimingSystems.SniperAimingSystem import SniperAimingSystem
 from AvatarInputHandler.AimingSystems.StrategicAimingSystem import StrategicAimingSystem
+import gui.Scaleform.daapi.view.battle.shared.crosshair.plugins as plug
 
-from xfw.events import registerEvent
+from xfw.events import registerEvent, overrideMethod
 import xvm_battle.python.battle as battle
 from xfw_actionscript.python import *
 from xvm_main.python.logger import *
@@ -14,11 +15,11 @@ ARCADE_MODE = 'arc'
 SNIPER_MODE = 'sn'
 STRATEGIC_MODE = 'str'
 SHIFT = 0.0775
-
+NETS_TYPES = {0: 'diagonal', 1: 'horizontal', 2: 'radial', 3: 'dotted'}
 
 aimMode = ARCADE_MODE
 y = 0.0
-
+netType = {ARCADE_MODE: 0, SNIPER_MODE: 0}
 
 @registerEvent(ArcadeAimingSystem, 'enable')
 def ArcadeAimingSystem_enable(self, targetPos, turretYaw=None, gunPitch=None):
@@ -64,6 +65,17 @@ def Vehicle_onEnterWorld(self, prereqs):
         as_event('ON_AIM_MODE')
 
 
+@overrideMethod(plug, '_makeSettingsVO')
+def plugins_makeSettingsVO(base, settingsCore, *keys):
+    data = base(settingsCore, *keys)
+    if 1 in data:
+        netType[ARCADE_MODE] = data[1].get('netType', None)
+    if 2 in data:
+        netType[SNIPER_MODE] = data[2].get('netType', None)
+    as_event('ON_AIM_MODE')
+    return data
+
+
 @xvm.export('aim.mode', deterministic=False)
 def aim_mode(arc=ARCADE_MODE, sn=SNIPER_MODE, strat=STRATEGIC_MODE):
     if aimMode == ARCADE_MODE:
@@ -77,3 +89,11 @@ def aim_mode(arc=ARCADE_MODE, sn=SNIPER_MODE, strat=STRATEGIC_MODE):
 @xvm.export('aim.y', deterministic=False)
 def aim_y(shift=0.0):
     return int(y + shift)
+
+
+@xvm.export('aim.netType', deterministic=False)
+def aim_netType(*args):
+    currentNet = netType.get(aimMode, None)
+    if currentNet is None:
+        return None
+    return args[currentNet] if currentNet < len(args) else NETS_TYPES[currentNet]
