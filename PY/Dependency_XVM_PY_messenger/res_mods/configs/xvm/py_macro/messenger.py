af32d04cd624a79204ef56c75bbec6a719fb2992
elektrosmoker <40739360+elektrosmoker@users.noreply.github.com>
2021-02-01 11:11:31 +0100

    upd messenger.py (maxlinescount=6)

--- PY/Dependency_XVM_PY_messenger/res_mods/configs/xvm/py_macro/messenger.py
+++ PY/Dependency_XVM_PY_messenger/res_mods/configs/xvm/py_macro/messenger.py
@@ -4,5 +4,5 @@ import messenger.gui.Scaleform.view.battle.messenger_view as messenger_view
 @overrideMethod(messenger_view, '_makeSettingsVO')
 def _makeSettingsVO(base,arenaVisitor):
     makeSettingsVO = base(arenaVisitor)
-    makeSettingsVO['maxLinesCount'] = 8
+    makeSettingsVO['maxLinesCount'] = 6
     return makeSettingsVO
