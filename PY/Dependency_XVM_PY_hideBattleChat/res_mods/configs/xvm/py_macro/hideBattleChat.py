f69a660b57155a7ad2cca9d34e8cd57ff882672c
elektrosmoker <40739360+elektrosmoker@users.noreply.github.com>
2021-06-09 17:56:51 +0200

    Update hideBattleChat.py

--- PY/Dependency_XVM_PY_hideBattleChat/res_mods/configs/xvm/py_macro/hideBattleChat.py
+++ PY/Dependency_XVM_PY_hideBattleChat/res_mods/configs/xvm/py_macro/hideBattleChat.py
@@ -1,24 +1,61 @@
 import game
 import Keys
+from messenger import g_settings
 from messenger.gui.Scaleform.battle_entry import BattleEntry
 from messenger import MessengerEntry
+from messenger.gui.Scaleform.channels.layout import BattleLayout
 
 import xvm_main.python.config as config
-from xfw.events import registerEvent
+from xfw.events import registerEvent, overrideMethod
 from xvm_main.python.logger import *
+
+
+MODIFIERS = {'none':  Keys.KEY_NONE,
+             'shift': Keys.MODIFIER_SHIFT,
+             'ctrl':  Keys.MODIFIER_CTRL,
+             'alt':   Keys.MODIFIER_ALT}
+
+isChatDisabled = False
 
 
 @registerEvent(game, 'handleKeyEvent', True)
 def game_handleKeyEvent(event):
-    hotkey = config.get('hotkeys/hideBattleChat', {'enabled': True, 'keyCode': Keys.KEY_H})
-    if hotkey.get('enabled', True):
-        gui = MessengerEntry.g_instance.gui
-        if gui.isFocused():
-            return
+    global isChatDisabled
+    hotkeyHide = config.get('hotkeys/hideBattleChat', {'enabled': True, 'keyCode': Keys.KEY_H, 'modifier': 'none'})
+    gui = MessengerEntry.g_instance.gui
+    if gui.isFocused():
+        return
+    if hotkeyHide.get('enabled', True):
         isDown, key, mods, isRepeat = game.convertKeyEvent(event)
-        if key == hotkey.get('keyCode', Keys.KEY_H) and isDown and not isRepeat:
+        if key == hotkeyHide.get('keyCode', Keys.KEY_H) and isDown and not isRepeat:
+            modifier = str(hotkeyHide.get('modifier', 'none')).lower()
+            if mods != MODIFIERS.get(modifier, 'none'):
+                return
             entry = gui._GUIDecorator__current()
             if isinstance(entry, BattleEntry):
                 view = entry._BattleEntry__view()
                 if view is not None:
                     view.flashObject.visible = not view.flashObject.visible
+
+    hotkeyDisable = config.get('hotkeys/disableBattleChat', {'enabled': True, 'keyCode': Keys.KEY_O, 'modifier': 'none'})
+    if hotkeyDisable.get('enabled', True):
+        isDown, key, mods, isRepeat = game.convertKeyEvent(event)
+        if key == hotkeyDisable.get('keyCode', Keys.KEY_O) and isDown and not isRepeat:
+            modifier = str(hotkeyDisable.get('modifier', 'none')).lower()
+            if mods != MODIFIERS.get(modifier, 'none'):
+                return
+            entry = gui._GUIDecorator__current()
+            if isinstance(entry, BattleEntry):
+                view = entry._BattleEntry__view()
+                if view is not None:
+                    isChatDisabled = not isChatDisabled
+                    g_settings.userPrefs._replace(disableBattleChat=isChatDisabled)
+                    view.invalidateReceivers()
+
+
+@overrideMethod(BattleLayout, 'isEnabled')
+def isEnabled(base, self):
+    if isChatDisabled:
+        return False
+    else:
+        return base(self)
